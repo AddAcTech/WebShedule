efca5f551a1d760fd35c8bb538762d0a25b6414b
AddAcTech <aguilar.chavez.alexis.daniel@gmail.com>
2024-01-23 15:45:48 -0600

    Header

--- src/components/Principal.jsx
+++ src/components/Principal.jsx
@@ -2,16 +2,17 @@ import React, { useEffect, useState } from "react";
 import Subject from "./Subject";
 import Header from "./Header";
 import EmptySchedule from "./EmptySchedule";
+import { AiFillSchedule } from "react-icons/ai";
 
 function Principal() {
   const [subjects, setSubjects] = useState([]);
-  /* useEffect(() => {
+  useEffect(() => {
     const response = JSON.parse(localStorage.getItem("schedule"));
     if (response) {
       setSubjects(response);
       console.log(response);
     }
-  }, []); */
+  }, []);
 
   if (subjects.length === 0 || !subjects) {
     return <EmptySchedule />;
@@ -21,6 +22,10 @@ function Principal() {
     <div className="h-screen flex flex-col">
       <Header />
       <div className="flex flex-col">
+        <div className="flex items-center gap-2 mx-auto">
+          <AiFillSchedule size={40} />
+          <h1 className="text-center text-3xl font-bold my-2">DAY schedule</h1>
+        </div>
         {subjects.map((subject, index) => (
           <Subject
             subject={subject.subject}
